4c2a406a1db05ab32366e6f203aa0ae228e8df0f
Manx98 <wenyiyu98@gmail.com>
2024-07-04 00:09:13 +0800

    移除修改

--- 天翼云盘.sh
+++ 天翼云盘.sh
@@ -1,10 +1,6 @@
 #!/usr/bin/env bash
 # cron:0 9 * * *
 # new Env("天翼网盘自动签到")
-cd "$(dirname "${BASH_SOURCE[0]}")" || {
-  echo "无法进入脚本运行目录"
-  exit -1
-}
 echo "开始安装依赖..."
 pip install -r requirements.txt || {
   echo "安装依赖失败"
