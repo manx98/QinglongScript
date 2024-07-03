cb2ddfb4b407e2c89b6ee4300fd7a1b61f0735e8
Manx98 <wenyiyu98@gmail.com>
2024-07-04 00:19:11 +0800

    进入工作目录

--- 天翼云盘.sh
+++ 天翼云盘.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 # cron:0 9 * * *
 # new Env("天翼网盘自动签到")
+cd /ql/data/repo/manx98_QinglongScript || {
+  echo "切换目录失败"
+  exit -1;
+}
 echo "开始安装依赖..."
 pip install -r requirements.txt || {
   echo "安装依赖失败"
